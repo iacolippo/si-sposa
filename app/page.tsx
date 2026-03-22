43e1fbd9af1e3b8c7931940c68bfcf0c78e61f39
Iacopo Poli <iacopo@lighton.ai>
2026-03-22 21:35:59 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -305,11 +305,11 @@ export default function Home() {
       {/* RSVP Section */}
       <section id="rsvp" className="pt-32 pb-12 md:py-20 bg-gradient-to-br from-bordeaux to-bordeaux/90 text-white relative overflow-hidden">
         {/* Corner ornaments */}
-        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="absolute -top-24 -left-6 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
-        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="absolute -top-24 -right-6 -scale-x-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
-        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="hidden md:block absolute bottom-0 -left-6 -scale-y-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
-        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="hidden md:block absolute bottom-0 -right-6 -scale-x-100 -scale-y-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
-        <div className="max-w-2xl mx-auto px-6 relative z-10">
+        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="absolute -top-24 -left-6 w-[200px] md:w-[400px] pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
+        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="absolute -top-24 -right-6 w-[200px] md:w-[400px] -scale-x-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
+        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="hidden md:block absolute bottom-0 -left-6 w-[400px] -scale-y-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
+        <Image src="/images/CORIC3.svg" alt="" width={400} height={400} className="hidden md:block absolute bottom-0 -right-6 w-[400px] -scale-x-100 -scale-y-100 pointer-events-none" style={{ filter: 'invert(1)', opacity: 0.8 }} />
+        <div className="max-w-2xl mx-auto px-8 md:px-6 relative z-10">
           <div className="text-center mb-8">
             <h2 className="text-4xl md:text-5xl font-serif text-crema mb-3">RSVP</h2>
             <div className="w-12 h-px bg-white/40 mx-auto mb-4"></div>
